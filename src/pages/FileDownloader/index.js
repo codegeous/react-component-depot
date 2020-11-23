304072762cb6ac842c28105b1833ffb93eaf9ffa
Prem Kumar <premkumarssp@gmail.com>
2020-11-23 19:45:43 +0400

    Fixed import issue

--- src/pages/FileDownloader/index.js
+++ src/pages/FileDownloader/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "components/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useFileDownloader from "hooks/useFileDownloader";
+import ExternalInfo from "components/ExternalInfo";
 
 const files = [
   {
